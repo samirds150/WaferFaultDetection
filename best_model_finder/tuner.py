660933d4b9686eda085b24aaab0cb9c2c3fedc91
Sam <sameer.easyr@gmail.com>
2022-07-14 16:48:26 +0530

    Updated tunerfile 1407

--- best_model_finder/tuner.py
+++ best_model_finder/tuner.py
@@ -1,6 +1,7 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
 from xgboost import XGBClassifier
+
 from sklearn.metrics  import roc_auc_score,accuracy_score
 
 class Model_Finder:
